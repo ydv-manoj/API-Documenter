e76d4b7279f225559f03cbbaa83c10e1d2a8f19f
ydv-manoj <mojirao770@gmail.com>
2025-06-18 17:33:31 +0530

    github workflow added

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'coverage/',
+    'docs/',
+    '*.min.js'
+  ]
+};
